7758add193273f5204a794f83c83da7d78f8afab
Chris <morenochristopher580@gmail.com>
2025-12-10 21:28:45 -0600

    Update boot.asm

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,1 +1,29 @@
+bits 32
+section .multiboot_header
+header_start:
+    dd 0xe85250d6
+    dd 0
+    dd header_end - header_start
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+    dw 0
+    dw 0
+    dd 8
+header_end:
 
+section .bss
+align 16
+stack_bottom:
+    resb 16384
+stack_top:
+
+section .text
+global _start
+extern kernel_main
+
+_start:
+    mov esp, stack_top
+    push ebx
+    push eax
+    cli
+    call kernel_main
+    hlt
