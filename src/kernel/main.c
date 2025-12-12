64af8769de41f493461c7424874265bf1b1bdd59
Chris <morenochristopher580@gmail.com>
2025-12-11 19:11:10 -0600

    Update main.c

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,20 +1,13 @@
 void kernel_main() {
-    // Direct VGA write - no functions, no headers
     volatile char *vga = (volatile char*)0xB8000;
     
-    // Write "NEKOOS" in center
-    vga[ (12 * 80 + 37) * 2 ] = 'N';
-    vga[ (12 * 80 + 38) * 2 ] = 'E';
-    vga[ (12 * 80 + 39) * 2 ] = 'K';
-    vga[ (12 * 80 + 40) * 2 ] = 'O';
-    vga[ (12 * 80 + 41) * 2 ] = 'O';
-    vga[ (12 * 80 + 42) * 2 ] = 'S';
+    // VGA Code Page 437 indices
+    char word[] = {0x4E, 0x45, 0x4B, 0x4F, 0x4F, 0x53}; // N E K O O S
     
-    // White text
     for(int i = 0; i < 6; i++) {
-        vga[ (12 * 80 + 37 + i) * 2 + 1 ] = 0x0F;
+        vga[(12 * 80 + 37 + i) * 2] = word[i];      // Character
+        vga[(12 * 80 + 37 + i) * 2 + 1] = 0x0F;     // White on black
     }
     
-    // Hang forever
     while(1) asm("hlt");
 }
