add229107e9837908e1d35ae8b3fad59db5cc0eb
Chris <morenochristopher580@gmail.com>
2026-01-13 21:21:06 -0600

    Update boot.asm

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,29 +1,50 @@
-bits 32
-section .multiboot_header
-header_start:
-    dd 0xe85250d6
-    dd 0
-    dd header_end - header_start
-    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
-    dw 0
-    dw 0
-    dd 8
-header_end:
+bits 16
+org 0x7C00
 
-section .bss
-align 16
-stack_bottom:
-    resb 16384
-stack_top:
+start:
+    cli                     ; Disable interrupts
+    cld                     ; Clear direction flag
+    
+    ; Set up stack
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    
+    ; Save boot drive
+    mov [boot_drive], dl
+    
+    ; Load kernel from disk
+    mov ah, 0x02            ; Read sectors
+    mov al, 15              ; Number of sectors to read
+    mov ch, 0               ; Cylinder
+    mov dh, 0               ; Head
+    mov cl, 2               ; Sector (1 is boot sector)
+    mov bx, 0x7E00          ; Load after bootloader
+    int 0x13
+    jc disk_error
+    
+    ; Jump to kernel
+    jmp 0x7E00
 
-section .text
-global _start
-extern kernel_main
+disk_error:
+    mov si, error_msg
+    call print_string
+    jmp $
 
-_start:
-    mov esp, stack_top
-    push ebx
-    push eax
-    cli
-    call kernel_main
-    hlt
+print_string:
+    lodsb
+    or al, al
+    jz .done
+    mov ah, 0x0E
+    int 0x10
+    jmp print_string
+    .done:
+    ret
+
+error_msg db "Disk error!", 0
+boot_drive db 0
+
+times 510-($-$$) db 0
+dw 0xAA55
