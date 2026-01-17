e03befb8d5ee6999cc24528436b680faabb33411
Chris <morenochristopher580@gmail.com>
2026-01-16 21:48:11 -0600

    Update terminal.c

--- src/kernel/terminal.c
+++ src/kernel/terminal.c
@@ -2,7 +2,6 @@
 #include "vga.h"
 #include "keyboard.h"
 #include "io.h"
-#include <string.h>    
 
 #define MAX_INPUT 256
 
@@ -136,4 +135,45 @@ void terminal_run_shell(void) {
     }
 }
 
-// REMOVED DUPLICATE STRING FUNCTIONS - USING string.h INSTEAD
+// ============= STRING FUNCTIONS (NO LIBC AVAILABLE) =============
+
+size_t strlen(const char* str) {
+    size_t len = 0;
+    while (str[len]) len++;
+    return len;
+}
+
+int strcmp(const char* s1, const char* s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return *(unsigned char*)s1 - *(unsigned char*)s2;
+}
+
+int strncmp(const char* s1, const char* s2, size_t n) {
+    while (n && *s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+        n--;
+    }
+    if (n == 0) return 0;
+    return *(unsigned char*)s1 - *(unsigned char*)s2;
+}
+
+void* memset(void* ptr, int value, size_t num) {
+    unsigned char* p = ptr;
+    while (num--) {
+        *p++ = value;
+    }
+    return ptr;
+}
+
+void* memcpy(void* dest, const void* src, size_t n) {
+    unsigned char* d = dest;
+    const unsigned char* s = src;
+    while (n--) {
+        *d++ = *s++;
+    }
+    return dest;
+}
