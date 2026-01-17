e9c1529bfd7b5d8becadd2aa806901a11ec3bdd9
Chris <morenochristopher580@gmail.com>
2026-01-16 20:44:05 -0600

    Create terminal.h

---
+++ src/kernel/terminal.h
@@ -0,0 +1,12 @@
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+void terminal_init(void);
+void terminal_putchar(char c);
+void terminal_write(const char* data, size_t size);
+void terminal_writestring(const char* data);
+void terminal_setcolor(uint8_t color);
+void terminal_clear(void);
+void terminal_run_shell(void);
+
+#endif
