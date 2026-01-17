71aed063117079b8f45704696a2c9c70b103014b
Chris <morenochristopher580@gmail.com>
2026-01-17 09:30:12 -0600

    Update multiboot.asm

--- src/boot/multiboot.asm
+++ src/boot/multiboot.asm
@@ -1,23 +1,50 @@
+bits 32
+
 section .multiboot
 align 4
 
 multiboot_header:
-    dd 0x1BADB002              ; Magic number
+    dd 0x1BADB002              ; Magic
+    dd 0x00000007              ; Flags: align + meminfo + framebuffer
+    dd -(0x1BADB002 + 0x00000007) ; Checksum
     
-    ; FLAGS: bit 0 = align modules, bit 1 = memory map, bit 2 = framebuffer
-    dd 0x00000007              ; CHANGED FROM 0x00000003
-    
-    dd -(0x1BADB002 + 0x00000007) ; Updated checksum
-    
-    ; Framebuffer request fields (must be set if bit 2 in flags is set)
+    ; Framebuffer request
     dd 0                       ; header_addr
-    dd 0                       ; load_addr  
+    dd 0                       ; load_addr
     dd 0                       ; load_end_addr
     dd 0                       ; bss_end_addr
     dd 0                       ; entry_addr
-    
-    ; Framebuffer mode request
     dd 0                       ; mode_type (0 = linear graphics)
     dd 1024                    ; width
     dd 768                     ; height
     dd 32                      ; depth (bpp)
+
+section .text
+global _start
+
+_start:
+    ; Set up stack
+    mov esp, stack_top
+    
+    ; Push Multiboot info
+    push ebx    ; mb_info
+    push eax    ; magic
+    
+    ; Clear direction flag
+    cld
+    
+    ; Call kernel
+    extern kernel_main
+    call kernel_main
+    
+    ; Hang if returns
+    cli
+.hang:
+    hlt
+    jmp .hang
+
+section .bss
+align 16
+stack_bottom:
+    resb 16384
+stack_top:
