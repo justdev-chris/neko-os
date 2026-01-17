75f6e9c92c0171c6b97f36f996776d1a6e7d617e
Chris <morenochristopher580@gmail.com>
2026-01-16 20:02:39 -0600

    Update multiboot.c

--- src/kernel/multiboot.c
+++ src/kernel/multiboot.c
@@ -1,39 +1,16 @@
-// src/kernel/multiboot.c
 #include <stdint.h>
 
-// Multiboot header must be in the first 8KB of the kernel
-__attribute__((section(".multiboot")))
-const uint32_t multiboot_header[] = {
-    // Magic number
-    0x1BADB002,
-    // Flags
-    0x00000003,  // ALIGN + MEMINFO
-    // Checksum
-    -(0x1BADB002 + 0x00000003),
-    // Header address
-    0x00000000,
-    // Load address  
-    0x00000000,
-    // Load end address
-    0x00000000,
-    // BSS end address
-    0x00000000,
-    // Entry address
-    0x00000000,
-    // Video mode
-    0x00000000,
-    // Width
-    0x00000000,
-    // Height
-    0x00000000,
-    // Depth
-    0x00000000
-};
-
-// Simple function to parse multiboot info (optional)
 void parse_multiboot_tags(uint32_t addr) {
-    // Your existing code here
-    uint32_t size = *(uint32_t*)addr;
-    (void)size;  // Mark as used to avoid warning
-    // ... rest of your code
+    uint32_t *tag = (uint32_t*)addr;
+    uint32_t type = tag[0];
+    uint32_t size = tag[1];
+    
+    (void)type;
+    (void)size;
+    
+    while (type != 0) {
+        tag = (uint32_t*)((uintptr_t)tag + ((size + 7) & ~7));
+        type = tag[0];
+        size = tag[1];
+    }
 }
