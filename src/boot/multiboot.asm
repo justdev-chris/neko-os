5d0b5839a2d89b3e6755d81e35a12b05068e723a
Chris <morenochristopher580@gmail.com>
2026-01-16 20:02:22 -0600

    Update multiboot.asm

--- src/boot/multiboot.asm
+++ src/boot/multiboot.asm
@@ -1,22 +1,16 @@
-; multiboot.asm
 section .multiboot
 align 4
 
-; Multiboot header
 multiboot_header:
-    dd 0x1BADB002              ; magic
-    dd 0x00000003              ; flags
-    dd -(0x1BADB002 + 0x00000003) ; checksum
-    
-    ; Optional fields (set to 0 since we don't need them)
-    dd 0x00000000              ; header_addr
-    dd 0x00000000              ; load_addr  
-    dd 0x00000000              ; load_end_addr
-    dd 0x00000000              ; bss_end_addr
-    dd 0x00000000              ; entry_addr
-    
-    ; Video mode (0 = linear graphics)
-    dd 0x00000000              ; mode_type
-    dd 0x00000000              ; width
-    dd 0x00000000              ; height
-    dd 0x00000000              ; depth
+    dd 0x1BADB002
+    dd 0x00000003
+    dd -(0x1BADB002 + 0x00000003)
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
+    dd 0x00000000
