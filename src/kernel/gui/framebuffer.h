f62334da8ff41cc8faa8d0fc1760c22bb7e329e8
Chris <morenochristopher580@gmail.com>
2026-01-17 09:41:13 -0600

    Update framebuffer.h

--- src/kernel/gui/framebuffer.h
+++ src/kernel/gui/framebuffer.h
@@ -7,5 +7,6 @@ void fb_init(uint32_t addr, uint32_t width, uint32_t height,
              uint32_t bpp, uint32_t pitch);
 void fb_clear(uint32_t color);
 void fb_draw_rect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t color);
+void fb_fill_rect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t color);
 
 #endif
