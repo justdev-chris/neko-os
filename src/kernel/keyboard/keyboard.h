0841b950b4928bd678ddc41ced3613b576b6f471
Chris <morenochristopher580@gmail.com>
2026-01-18 14:12:05 -0600

    Update keyboard.h

--- src/kernel/keyboard/keyboard.h
+++ src/kernel/keyboard/keyboard.h
@@ -4,8 +4,7 @@
 #include <stdint.h>
 
 void keyboard_init(void);
-uint8_t keyboard_get_scancode(void);
-void keyboard_handle_scancode(uint8_t scancode);
+uint8_t keyboard_get_scancode(void);  // Make sure this is here!
 char keyboard_getchar(void);
 
 #endif
