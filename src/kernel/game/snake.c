320121edc09cdbe9c4d386be4011e0c9095a407f
Chris <morenochristopher580@gmail.com>
2026-01-18 14:20:09 -0600

    Update snake.c

--- src/kernel/game/snake.c
+++ src/kernel/game/snake.c
@@ -1,201 +1,222 @@
 #include "snake.h"
 #include "../vga.h"
 #include "../keyboard/keyboard.h"
+#include "../io.h"
+#include <stddef.h>
 
-#define SNAKE_WIDTH 40
-#define SNAKE_HEIGHT 20
-#define MAX_SNAKE_LENGTH 100
+#define WIDTH 40
+#define HEIGHT 20
+#define MAX_LENGTH 100
 
-typedef struct {
-    int x, y;
-} Point;
-
-static Point snake[MAX_SNAKE_LENGTH];
+static int snake_x[MAX_LENGTH];
+static int snake_y[MAX_LENGTH];
 static int snake_length = 3;
-static int direction = 0;
-static Point food;
-static int score = 0;
+static int direction = 0; // 0=right, 1=down, 2=left, 3=up
+static int apple_x, apple_y;
 static int game_over = 0;
 
-// Simple random
-static unsigned int seed = 12345;
-static int simple_rand(void) {
-    seed = (seed * 1103515245 + 12345) & 0x7fffffff;
-    return seed;
+// Simple delay function
+static void delay(int ticks) {
+    for (volatile int i = 0; i < ticks * 10000; i++);
 }
 
-// Renamed to avoid conflict
-void snake_draw_border(void) {
-    vga_set_color(0x0F);
+static void place_apple(void) {
+    apple_x = (apple_x * 1103515245 + 12345) % (WIDTH - 2) + 1;
+    apple_y = (apple_y * 1103515245 + 12345) % (HEIGHT - 2) + 1;
     
-    for (int x = 0; x < SNAKE_WIDTH + 2; x++) {
-        vga_putchar_at(x, 0, '#');
-        vga_putchar_at(x, SNAKE_HEIGHT + 1, '#');
-    }
-    
-    for (int y = 1; y < SNAKE_HEIGHT + 1; y++) {
-        vga_putchar_at(0, y, '#');
-        vga_putchar_at(SNAKE_WIDTH + 1, y, '#');
-    }
-}
-
-void draw_snake(void) {
-    vga_set_color(0x0A);
-    vga_putchar_at(snake[0].x + 1, snake[0].y + 1, '@');
-    
-    vga_set_color(0x02);
-    for (int i = 1; i < snake_length; i++) {
-        vga_putchar_at(snake[i].x + 1, snake[i].y + 1, 'o');
-    }
-}
-
-void draw_food(void) {
-    vga_set_color(0x0C);
-    vga_putchar_at(food.x + 1, food.y + 1, '*');
-}
-
-void snake_init(void) {
-    for (int i = 0; i < 3; i++) {
-        snake[i].x = 10 - i;
-        snake[i].y = 10;
-    }
-    snake_length = 3;
-    
-    food.x = 20;
-    food.y = 10;
-    
-    direction = 0;
-    score = 0;
-    game_over = 0;
-    
-    vga_clear();
-    
-    vga_set_color(0x0E);
-    vga_puts_at(0, SNAKE_HEIGHT + 3, "Snake - WASD to move, Q to quit");
-    vga_puts_at(0, SNAKE_HEIGHT + 4, "Score: 0");
-    
-    snake_draw_border();
-    draw_snake();
-    draw_food();
-}
-
-void spawn_food(void) {
-    int valid = 0;
-    while (!valid) {
-        food.x = simple_rand() % SNAKE_WIDTH;
-        food.y = simple_rand() % SNAKE_HEIGHT;
-        
-        valid = 1;
-        for (int i = 0; i < snake_length; i++) {
-            if (snake[i].x == food.x && snake[i].y == food.y) {
-                valid = 0;
-                break;
-            }
+    // Make sure apple doesn't spawn on snake
+    for (int i = 0; i < snake_length; i++) {
+        if (snake_x[i] == apple_x && snake_y[i] == apple_y) {
+            place_apple(); // Try again
+            return;
         }
     }
 }
 
-void snake_update(void) {
-    if (game_over) return;
+static void init_game(void) {
+    // Initialize snake in middle
+    snake_length = 3;
+    for (int i = 0; i < snake_length; i++) {
+        snake_x[i] = WIDTH/2 - i;
+        snake_y[i] = HEIGHT/2;
+    }
     
+    direction = 0;
+    game_over = 0;
+    
+    // Place first apple
+    apple_x = WIDTH/2 + 5;
+    apple_y = HEIGHT/2;
+    place_apple();
+}
+
+static void draw_border(void) {
+    vga_set_color(0x0F);
+    
+    // Top and bottom borders
+    for (int x = 0; x < WIDTH; x++) {
+        vga_set_cursor(x, 0);
+        vga_putchar('#');
+        vga_set_cursor(x, HEIGHT-1);
+        vga_putchar('#');
+    }
+    
+    // Left and right borders
+    for (int y = 1; y < HEIGHT-1; y++) {
+        vga_set_cursor(0, y);
+        vga_putchar('#');
+        vga_set_cursor(WIDTH-1, y);
+        vga_putchar('#');
+    }
+}
+
+static void draw_game(void) {
+    // Clear play area
+    for (int y = 1; y < HEIGHT-1; y++) {
+        for (int x = 1; x < WIDTH-1; x++) {
+            vga_set_cursor(x, y);
+            vga_putchar(' ');
+        }
+    }
+    
+    // Draw snake
+    vga_set_color(0x0A); // Green head
+    vga_set_cursor(snake_x[0], snake_y[0]);
+    vga_putchar('@');
+    
+    vga_set_color(0x02); // Green body
+    for (int i = 1; i < snake_length; i++) {
+        vga_set_cursor(snake_x[i], snake_y[i]);
+        vga_putchar('O');
+    }
+    
+    // Draw apple
+    vga_set_color(0x0C); // Red
+    vga_set_cursor(apple_x, apple_y);
+    vga_putchar('*');
+    
+    // Draw score
+    vga_set_color(0x0E);
+    vga_set_cursor(0, HEIGHT);
+    vga_puts("Score: ");
+    // Simple score display
+    for (int i = 0; i < (snake_length - 3); i++) {
+        vga_putchar('*');
+    }
+    
+    vga_set_cursor(20, HEIGHT);
+    vga_puts("WASD to move, Q to quit");
+}
+
+static void move_snake(void) {
+    // Move body segments (from tail to head)
     for (int i = snake_length - 1; i > 0; i--) {
-        snake[i] = snake[i - 1];
+        snake_x[i] = snake_x[i-1];
+        snake_y[i] = snake_y[i-1];
     }
     
+    // Move head based on direction
     switch (direction) {
-        case 0: snake[0].x++; break;
-        case 1: snake[0].y++; break;
-        case 2: snake[0].x--; break;
-        case 3: snake[0].y--; break;
+        case 0: snake_x[0]++; break; // Right
+        case 1: snake_y[0]++; break; // Down
+        case 2: snake_x[0]--; break; // Left
+        case 3: snake_y[0]--; break; // Up
     }
-    
-    if (snake[0].x < 0 || snake[0].x >= SNAKE_WIDTH ||
-        snake[0].y < 0 || snake[0].y >= SNAKE_HEIGHT) {
+}
+
+static void check_collisions(void) {
+    // Wall collision
+    if (snake_x[0] <= 0 || snake_x[0] >= WIDTH-1 ||
+        snake_y[0] <= 0 || snake_y[0] >= HEIGHT-1) {
         game_over = 1;
         return;
     }
     
+    // Self collision
     for (int i = 1; i < snake_length; i++) {
-        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+        if (snake_x[0] == snake_x[i] && snake_y[0] == snake_y[i]) {
             game_over = 1;
             return;
         }
     }
     
-    if (snake[0].x == food.x && snake[0].y == food.y) {
-        score += 10;
-        snake_length++;
-        if (snake_length > MAX_SNAKE_LENGTH) snake_length = MAX_SNAKE_LENGTH;
-        spawn_food();
-        
-        // Update score
-        vga_set_color(0x0E);
-        vga_puts_at(7, SNAKE_HEIGHT + 4, "      ");
-        
-        // Convert score to string
-        char score_str[10];
-        int n = score, i = 0;
-        if (n == 0) score_str[i++] = '0';
-        while (n > 0) {
-            score_str[i++] = '0' + (n % 10);
-            n /= 10;
+    // Apple collision
+    if (snake_x[0] == apple_x && snake_y[0] == apple_y) {
+        // Grow snake (only if we have room)
+        if (snake_length < MAX_LENGTH) {
+            snake_x[snake_length] = snake_x[snake_length-1];
+            snake_y[snake_length] = snake_y[snake_length-1];
+            snake_length++;
         }
-        score_str[i] = '\0';
-        for (int j = 0; j < i/2; j++) {
-            char temp = score_str[j];
-            score_str[j] = score_str[i-j-1];
-            score_str[i-j-1] = temp;
-        }
-        vga_puts_at(7, SNAKE_HEIGHT + 4, score_str);
+        place_apple();
     }
 }
 
-void snake_handle_input(char key) {
-    switch (key) {
-        case 'w': case 'W': if (direction != 1) direction = 3; break;
-        case 's': case 'S': if (direction != 3) direction = 1; break;
-        case 'a': case 'A': if (direction != 0) direction = 2; break;
-        case 'd': case 'D': if (direction != 2) direction = 0; break;
-        case 'q': case 'Q': game_over = 1; break;
+static void handle_input(void) {
+    uint8_t scancode = keyboard_get_scancode();
+    
+    if (!scancode) return;
+    
+    // Only process key press (not release)
+    if (scancode & 0x80) return;
+    
+    switch (scancode) {
+        case 0x11: // W
+            if (direction != 1) direction = 3;
+            break;
+        case 0x1F: // S
+            if (direction != 3) direction = 1;
+            break;
+        case 0x1E: // A
+            if (direction != 0) direction = 2;
+            break;
+        case 0x20: // D
+            if (direction != 2) direction = 0;
+            break;
+        case 0x10: // Q
+            game_over = 1;
+            break;
     }
 }
 
 void snake_run(void) {
-    snake_init();
+    vga_clear();
+    init_game();
+    
+    vga_set_color(0x0E);
+    vga_set_cursor(WIDTH/2 - 5, 0);
+    vga_puts("=== SNAKE ===");
     
     while (!game_over) {
-        char key = keyboard_getchar();
-        if (key) snake_handle_input(key);
+        draw_border();
+        draw_game();
         
-        snake_update();
-        snake_draw_border();
-        draw_snake();
-        draw_food();
+        handle_input();
+        move_snake();
+        check_collisions();
         
-        for (int i = 0; i < 1000000; i++) asm volatile ("nop");
+        // DELAY - this fixes the speed issue
+        delay(100);
     }
     
-    // Game over
+    // Game over screen
+    vga_clear();
     vga_set_color(0x0C);
-    vga_puts_at(SNAKE_WIDTH/2 - 5, SNAKE_HEIGHT/2, "GAME OVER");
+    vga_set_cursor(WIDTH/2 - 5, HEIGHT/2);
+    vga_puts("GAME OVER!");
     
-    char score_str[10];
-    int n = score, i = 0;
-    if (n == 0) score_str[i++] = '0';
-    while (n > 0) {
-        score_str[i++] = '0' + (n % 10);
-        n /= 10;
-    }
-    score_str[i] = '\0';
-    for (int j = 0; j < i/2; j++) {
-        char temp = score_str[j];
-        score_str[j] = score_str[i-j-1];
-        score_str[i-j-1] = temp;
+    vga_set_color(0x0F);
+    vga_set_cursor(WIDTH/2 - 8, HEIGHT/2 + 1);
+    vga_puts("Score: ");
+    for (int i = 0; i < (snake_length - 3); i++) {
+        vga_putchar('*');
     }
     
-    vga_puts_at(SNAKE_WIDTH/2 - 8, SNAKE_HEIGHT/2 + 1, "Score: ");
-    vga_puts_at(SNAKE_WIDTH/2 - 1, SNAKE_HEIGHT/2 + 1, score_str);
-    vga_puts_at(SNAKE_WIDTH/2 - 8, SNAKE_HEIGHT/2 + 2, "Press any key...");
+    vga_set_cursor(WIDTH/2 - 10, HEIGHT/2 + 3);
+    vga_puts("Press any key to exit");
     
-    while (!keyboard_getchar());
+    // Wait for key press
+    while (!keyboard_get_scancode()) {
+        delay(10);
+    }
 }
