e2ad3ca93aae6fd19ac5c4fc325ab4ab2f15aa5d
Chris <morenochristopher580@gmail.com>
2026-02-15 15:06:35 -0600

    Update memory.h

--- src/kernel/memory/memory.h
+++ src/kernel/memory/memory.h
@@ -3,19 +3,10 @@
 
 #include <stdint.h>
 
-// Initialize memory management
 void memory_init(unsigned long mem_lower, unsigned long mem_upper);
-
-// Get total memory in bytes
 unsigned long memory_get_total(void);
-
-// Get used memory in bytes
 unsigned long memory_get_used(void);
-
-// Allocate memory
 void* malloc(unsigned long size);
-
-// Free memory
 void free(void* ptr);
 
 #endif
