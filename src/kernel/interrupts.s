049a33f90e50da94f5067a97830afb2ff4ef7712
Chris <morenochristopher580@gmail.com>
2026-02-15 14:43:39 -0600

    Update interrupts.s

--- src/kernel/interrupts.s
+++ src/kernel/interrupts.s
@@ -1,37 +1,38 @@
+/* interrupts.S - GAS syntax */
 .section .text
 
-# Export symbols
+/* Export symbols */
 .global idt_load
 .global irq0_handler
 .global irq1_handler
 
-# IDT load function
+/* IDT load function */
 idt_load:
-    movl 4(%esp), %eax    # Get IDT pointer
-    lidt (%eax)           # Load IDT
+    movl 4(%esp), %eax    /* Get IDT pointer */
+    lidt (%eax)           /* Load IDT */
     ret
 
-# Common interrupt stub macro
+/* Macro for interrupt stubs */
 .macro IRQ_STUB num
 irq\num\()_handler:
-    pushal                # Push all general purpose registers
-    pushl %ds             # Push data segment
-    pushl %es             # Push extra segment
-    pushl %fs             # Push FS segment
-    pushl %gs             # Push GS segment
+    pushal                /* Push all general purpose registers */
+    pushl %ds             /* Push data segment */
+    pushl %es             /* Push extra segment */
+    pushl %fs             /* Push FS segment */
+    pushl %gs             /* Push GS segment */
     
-    # Call C handler
+    /* Call C handler */
     call irq\num\()_handler_c
     
-    popl %gs              # Restore segments
+    popl %gs              /* Restore segments */
     popl %fs
     popl %es
     popl %ds
-    popal                 # Restore registers
+    popal                 /* Restore registers */
     
-    iret                  # Return from interrupt
+    iret                  /* Return from interrupt */
 .endm
 
-# Generate IRQ stubs
+/* Generate IRQ stubs */
 IRQ_STUB 0
 IRQ_STUB 1
