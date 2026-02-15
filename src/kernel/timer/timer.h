af36884b952eb4630bdcd5cb39fcb8503f98668c
Chris <morenochristopher580@gmail.com>
2026-02-15 15:06:11 -0600

    Update timer.h

--- src/kernel/timer/timer.h
+++ src/kernel/timer/timer.h
@@ -3,19 +3,10 @@
 
 #include <stdint.h>
 
-// Initialize PIT timer with specified frequency
 void timer_init(uint32_t frequency);
-
-// Timer interrupt handler (called from IRQ0)
 void timer_handler(void);
-
-// Get current system ticks (increments ~100 times per second)
 uint32_t timer_get_ticks(void);
-
-// Get system uptime in seconds
 uint32_t timer_get_seconds(void);
-
-// Wait for specified number of milliseconds (blocking)
 void timer_sleep(uint32_t milliseconds);
 
 #endif
