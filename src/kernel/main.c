a18fc9f5e9a78aee291cffdd571e94e0088eb9e5
Chris <morenochristopher580@gmail.com>
2026-02-15 15:03:02 -0600

    Update main.c

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -5,7 +5,7 @@
 #include "game/snake.h"
 #include "timer/timer.h"
 #include "memory/memory.h"
-#include "io.h"  // Make sure this is included for outb
+#include "io.h"
 #include <stdint.h>
 
 struct multiboot_info {
@@ -38,7 +38,6 @@ struct multiboot_info {
     uint8_t reserved;
 } __attribute__((packed));
 
-// IDT entry structure
 struct idt_entry {
     uint16_t base_low;
     uint16_t selector;
@@ -47,40 +46,18 @@ struct idt_entry {
     uint16_t base_high;
 } __attribute__((packed));
 
-// IDT pointer structure
 struct idt_ptr {
     uint16_t limit;
     uint32_t base;
 } __attribute__((packed));
 
-// IDT and handlers
 #define IDT_ENTRIES 256
 struct idt_entry idt[IDT_ENTRIES];
 struct idt_ptr idtp;
 
-// External assembly functions
 extern void idt_load(struct idt_ptr*);
 extern void irq0_handler(void);
 extern void irq1_handler(void);
-
-// Function prototypes
-void idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags);
-void idt_install(void);
-void irq_remap(void);
-void irq_install(void);
-
-// C handler wrappers - NOT static so they can be called from assembly
-void irq0_handler_c(void) {
-    timer_handler();
-    // Send EOI to master PIC
-    outb(0x20, 0x20);
-}
-
-void irq1_handler_c(void) {
-    keyboard_handler();
-    // Send EOI to master PIC
-    outb(0x20, 0x20);
-}
 
 void idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags) {
     idt[num].base_low = base & 0xFFFF;
@@ -94,39 +71,40 @@ void idt_install(void) {
     idtp.limit = (sizeof(struct idt_entry) * IDT_ENTRIES) - 1;
     idtp.base = (uint32_t)&idt;
     
-    // Clear IDT
     for (int i = 0; i < IDT_ENTRIES; i++) {
         idt_set_gate(i, 0, 0, 0);
     }
     
-    // Load IDT
     idt_load(&idtp);
 }
 
 void irq_remap(void) {
-    // Remap IRQ table to 0x20-0x2F (protected mode standard)
-    outb(0x20, 0x11);  // Start init of master PIC
-    outb(0xA0, 0x11);  // Start init of slave PIC
-    outb(0x21, 0x20);  // Master PIC vector offset -> 0x20
-    outb(0xA1, 0x28);  // Slave PIC vector offset -> 0x28
-    outb(0x21, 0x04);  // Tell master there's a slave at IRQ2
-    outb(0xA1, 0x02);  // Tell slave its cascade identity
-    outb(0x21, 0x01);  // 8086 mode for master
-    outb(0xA1, 0x01);  // 8086 mode for slave
-    
-    // Clear masks
-    outb(0x21, 0xFD);  // Enable IRQ1 (keyboard) only, mask others
-    outb(0xA1, 0xFF);  // Mask all slave IRQs
+    outb(0x20, 0x11);
+    outb(0xA0, 0x11);
+    outb(0x21, 0x20);
+    outb(0xA1, 0x28);
+    outb(0x21, 0x04);
+    outb(0xA1, 0x02);
+    outb(0x21, 0x01);
+    outb(0xA1, 0x01);
+    outb(0x21, 0xFD);
+    outb(0xA1, 0xFF);
 }
 
 void irq_install(void) {
     irq_remap();
-    
-    // Install IRQ handlers
-    idt_set_gate(32, (uint32_t)irq0_handler, 0x08, 0x8E);  // Timer
-    idt_set_gate(33, (uint32_t)irq1_handler, 0x08, 0x8E);  // Keyboard
-    
-    asm volatile("sti");  // Enable interrupts
+    idt_set_gate(32, (uint32_t)irq0_handler, 0x08, 0x8E);
+    idt_set_gate(33, (uint32_t)irq1_handler, 0x08, 0x8E);
+}
+
+void irq0_handler_c(void) {
+    timer_handler();
+    outb(0x20, 0x20);
+}
+
+void irq1_handler_c(void) {
+    keyboard_handler();
+    outb(0x20, 0x20);
 }
 
 void print_banner(void) {
@@ -145,36 +123,32 @@ void print_banner(void) {
     vga_puts("[OK] Keyboard driver\n");
     vga_puts("[OK] Terminal shell\n");
     vga_puts("[OK] Snake game\n");
-    vga_set_color(0x0C);
     vga_puts("[OK] Timer driver\n");
     vga_puts("[OK] IRQ handlers\n\n");
+    
+    vga_set_color(0x0F);
 }
 
 void run_text_mode(void) {
     vga_set_color(0x0F);
     vga_puts("Type 'help' for commands\n\n");
-    
     terminal_run_shell();
 }
 
 void kernel_main(uint32_t magic, uint32_t mb_info_addr) {
-    (void)magic;  // Suppress unused parameter warning
-    (void)mb_info_addr;  // Suppress unused parameter warning
+    (void)magic;
+    (void)mb_info_addr;
     
     vga_init();
-    
-    // Install IDT and IRQs first
     idt_install();
     irq_install();
-    
-    // Initialize PIT timer with 100 Hz
     timer_init(100);
-    
-    // Initialize memory (using multiboot info would be better, but simple for now)
-    memory_init(0, 32 * 1024 * 1024);  // Assume 32MB of memory
-    
+    memory_init(0, 32 * 1024 * 1024);
     print_banner();
     keyboard_init();
+    
+    asm volatile("sti");
+    
     run_text_mode();
     
     while (1) asm volatile("hlt");
