1a54c61b3ccc104559d19282fd2bd9ba425aae73
Chris <morenochristopher580@gmail.com>
2026-02-16 17:48:15 -0600

    Update multiboot.asm

--- src/boot/multiboot.asm
+++ src/boot/multiboot.asm
@@ -1,42 +1,30 @@
-bits 32
-
 section .multiboot
 align 4
-
-multiboot_header:
-    dd 0x1BADB002              ; Magic
-    dd 0x00000003              ; Flags: align + meminfo ONLY (no framebuffer)
-    dd -(0x1BADB002 + 0x00000003) ; Checksum
+    dd 0x1BADB002           ; Magic number
+    dd 0x00000007           ; Flags: page align + memory info + video mode
+    dd -(0x1BADB002 + 0x00000007) ; Checksum
     
-    ; NO FRAMEBUFFER REQUEST SECTION
-    ; (remove the framebuffer settings)
+    ; Multiboot header extensions for framebuffer
+    dd 0                    ; header_addr
+    dd 0                    ; load_addr
+    dd 0                    ; load_end_addr
+    dd 0                    ; bss_end_addr
+    dd 0                    ; entry_addr
+    dd 0                    ; mode_type (0 = linear graphics)
+    dd 1024                 ; width
+    dd 768                  ; height
+    dd 32                   ; depth (bits per pixel)
 
 section .text
-global _start
+global start
+extern kernel_main
 
-_start:
-    ; Set up stack
-    mov esp, stack_top
-    
-    ; Push Multiboot info
-    push ebx    ; mb_info
-    push eax    ; magic
-    
-    ; Clear direction flag
-    cld
-    
-    ; Call kernel
-    extern kernel_main
+start:
+    push ebx                ; Push multiboot info pointer
+    push eax                ; Push magic number
     call kernel_main
-    
-    ; Hang if returns
-    cli
-.hang:
-    hlt
-    jmp .hang
 
-section .bss
-align 16
-stack_bottom:
-    resb 16384
-stack_top:
+halt:
+    cli
+    hlt
+    jmp halt
