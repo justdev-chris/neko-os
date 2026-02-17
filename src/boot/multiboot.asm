93abbd18e835e4e58196ab1279c579140681adcb
Chris <morenochristopher580@gmail.com>
2026-02-16 20:53:56 -0600

    Update multiboot.asm

--- src/boot/multiboot.asm
+++ src/boot/multiboot.asm
@@ -1,19 +1,8 @@
 section .multiboot
 align 4
     dd 0x1BADB002           ; Magic number
-    dd 0x00000007           ; Flags: page align + memory info + video mode
-    dd -(0x1BADB002 + 0x00000007) ; Checksum
-    
-    ; Multiboot header extensions for framebuffer
-    dd 0                    ; header_addr
-    dd 0                    ; load_addr
-    dd 0                    ; load_end_addr
-    dd 0                    ; bss_end_addr
-    dd 0                    ; entry_addr
-    dd 0                    ; mode_type (0 = linear graphics)
-    dd 1024                 ; width
-    dd 768                  ; height
-    dd 32                   ; depth (bits per pixel)
+    dd 0x00000003           ; Flags: page align + memory info (NO video mode)
+    dd -(0x1BADB002 + 0x00000003) ; Checksum
 
 section .text
 global start
